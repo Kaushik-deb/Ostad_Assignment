e11d5dd11768b7270bb068fe76f6ea16433b6339
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-20 10:31:08 +0600

    new

---
+++ task03.php
@@ -0,0 +1,25 @@
+<?php
+$very_old=0;
+$old=1;
+$new=1;
+for($i=0;$i<10;$i++){
+    if($very_old>100){
+        break;
+    };
+    echo "$very_old";
+    echo PHP_EOL;
+    $new=$old+$very_old;
+    $very_old=$old;
+    $old=$new;
+}
+
+
+
+
+
+
+
+
+
+
+?>
