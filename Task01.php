319fb820407f9d61b15e5426bf6636d078243936
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-20 10:13:30 +0600

    new

--- Task01.php
+++ Task01.php
@@ -1,9 +1,26 @@
 <?php
-function is_even($start,$end,$step){
-   for($i=0;$i<=$end;$i+=$step){
-    echo $i;
+// function is_even($start,$end,$step){
+//    for($i=0;$i<=$end;$i+=$step){
+//     echo $i;
+//     echo PHP_EOL;
+//    }
+// }
+// is_even(1,20,2);
+// ?>
+<?php
+// $num = 1;
+// while($num++<=20){
+//     echo $num;
+//     $num++;
+//     echo PHP_EOL;
+// }
+// ?>
+<?php
+$num = 1;
+do{
+    echo ++$num;
     echo PHP_EOL;
-   }
+    $num++;
 }
-is_even(1,20,2);
+while($num<=20);
 ?>
