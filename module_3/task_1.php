961ddd2fee93b47801c46aedaea1b1469ae53ffc
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-25 21:11:45 +0600

    new

---
+++ module_3/task_1.php
@@ -0,0 +1,38 @@
+<?php
+
+// Create a string variable called $text with the value "The quick brown fox jumps over the lazy dog.". Write a PHP function which takes "$text" as an argument to:
+// Convert the string to all lowercase.
+// Replace "brown" with "red" in the string.
+// Print the modified text.
+
+
+
+function modifiedText($text){
+    $lowerCase=strtolower($text);
+    $replaced=str_ireplace("brown","red",$lowerCase);
+    echo $replaced;
+}
+
+$text = "The quick brown fox jumps over the lazy dog.";
+
+modifiedText($text);
+
+
+// $text = "The quick brown fox jumps over the lazy dog.";
+
+// $newText=strtolower($text);
+// $replaced=str_ireplace("brown","red",$text);
+// echo $newText;
+// echo $replaced;
+
+
+
+
+
+
+
+
+
+
+
+?>
